be1660ba488828922c0af7e005f923b5d7bbb4df
VLinkus <LinkusVyte@gmail.com>
2023-12-03 10:40:58 +0200

    M3-Task6 Completed

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int rounded = (int) (numberToBeRounded * 10);
+        int result  = rounded%10 >=5 ? rounded/10+1 : rounded/10;
+        System.out.println(result);
     }
 }
